7eebb82863fe55e6f81ebd79954e273349de8016
Gerald Adjei <geraldsix89@gmail.com>
2024-04-12 23:52:47 +0100

    npm package

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,18 @@ import withVideos from "next-videos";
 
 const nextConfig = {
   /* config options here */
+  output: "export",
 };
 
 export default {
   ...nextConfig,
   ...withVideos(),
 };
+
+// module.exports = {
+//   // Add the output configuration
+//   experimental: {
+//     output: "export",
+//   },
+//   // Other Next.js configurations can go here
+// };
